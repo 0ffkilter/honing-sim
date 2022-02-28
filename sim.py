b1df33322a975783a89f13134e1641925550bc0c
Matt Gee <geem2014@gmail.com>
2022-02-28 10:25:03 -0800

    code cleanup + median

--- sim.py
+++ sim.py
@@ -37,15 +37,13 @@ def get_values(dictionary, graces, blessings, protections):
     return dictionary['grace'] * graces + dictionary['blessing'] * blessings + dictionary['protection'] * protections
 
 def sim(base_cost, base_percent, artisan_base, pity, graces=0, blessings=0, protections=0):
-
-    enchanted = False 
     total_cost = 0
 
     success_chance = base_percent
 
     current_artisans = 0
 
-    for i in range(100):
+    while True:
 
         if current_artisans >= 100:
             return total_cost + base_cost
@@ -73,5 +71,6 @@ trials = [
 sim_number = 100000
 
 for t in trials:
-    print(t, numpy.average([sim(base_cost, base_percent,  artisans, pity, t[0], t[1], t[2]) for x in range(sim_number)]))
+    trial = [sim(base_cost, base_percent, artisans, pity, t[0], t[1], t[2]) for x in range(sim_number)]
+    print(t, numpy.average(trial), numpy.median(trial))
 
